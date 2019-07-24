8e40dd2b1c2f62027b93df81993715bbbe63123d
kypanghao <panghao@221data.com>
2019-07-24 17:46:51 +0800

    delete useless files

--- src/main/java/i221/service/TrackService.java
+++ src/main/java/i221/service/TrackService.java
@@ -2,10 +2,6 @@ package i221.service;
 
 import i221.entity.TrackException;
 import i221.entity.VideoControlTask;
-import i221.entity.VideoImage;
-
-import java.awt.*;
-
 public interface TrackService {
     void trackFromRtsp(VideoControlTask videoControlTask) throws TrackException;
     void stopTrack(VideoControlTask videoControlTask) throws TrackException;
